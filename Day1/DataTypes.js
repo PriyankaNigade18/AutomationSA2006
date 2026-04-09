a3bbe6cfa68a7b69628fbf9869beb368963f2966
PriyankaNigade18 <piyu1818@gmail.com>
2026-04-09 22:29:35 +0530

    Javascript Day-1

---
+++ Day1/DataTypes.js
@@ -0,0 +1,40 @@
+/*
+Variable:
+----------------
+Variable is name of storage where we can store the data
+
+Keywords to declare variable
+-----------------------------
+1.var
+2.let(Mutable)
+3.const(Immutable)
+
+In Js (Modern Js ) we use 2 keywords to declare variable(let or const)
+
+syntaX:
+--------
+let vraiableName=value;
+
+DataTypes
+--------------
+Datatype help to decscribe what type of data we stire into variable
+
+1.Number
+2.String
+3.Boolean
+4.undefined
+5.null
+
+6.BigInt(ES6)NA for Automation
+7.symbol(ES6)NA for Automation
+
+
+*/
+
+let num1=90;
+console.log(num1);
+
+const pi=3.14;
+console.log(pi);
+
+
---
+++ Day1/HelloCode.js
@@ -0,0 +1,21 @@
+
+
+
+//Single line comment
+
+/*
+Multiple line comment
+
+How to print any statement in Js
+--------------------------------------
+console.log("statment");
+
+*/
+
+
+console.log("Hello Everyone!")
+
+console.log("Welcome to learn Javascript!")
+
+
+
